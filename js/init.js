4a55aa5193aa5671ddb0e2aec28646d045c47697
Form98 <80988819+Form98@users.noreply.github.com>
2021-03-22 21:46:42 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,14 @@
-/*  Initializa Firebase con la configuración del proyecto. Revisa la
- * configuración en tu servidor de Firebase.  */
-// @ts-ignore
-firebase.initializeApp({
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.3.1/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
     apiKey: "AIzaSyBNeBIUnujYa2ZAMR-VFkXrUNG64Cr9T5U",
     authDomain: "autentifica43ador-57aea.firebaseapp.com",
     projectId: "autentifica43ador-57aea",
@@ -9,4 +16,8 @@ firebase.initializeApp({
     messagingSenderId: "273140627142",
     appId: "1:273140627142:web:0348c6e02a6461eead49c7",
     measurementId: "G-4QXP5B9BCQ"
-    });
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
